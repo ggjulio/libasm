444adf774d3abaf1a1d6a301847f6b7b4ed5e7cc
ggjulio <juligonz@student.42.fr>
2020-08-02 15:35:13 +0200

    .

--- libasm.h
+++ libasm.h
@@ -6,7 +6,7 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/22 13:10:24 by juligonz          #+#    #+#             */
-/*   Updated: 2020/05/22 16:50:27 by juligonz         ###   ########.fr       */
+/*   Updated: 2020/08/02 15:22:08 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,18 @@
 
 # include <unistd.h>
 
-size_t		ft_strlen(const char *s);
-char		*ft_strcpy(char *dest, const char *src);
-int			ft_strcmp(const char *s1, const char *s2);
+size_t		ft_strlen(const char *s) asm("_ft_strlen");
+char		*ft_strcpy(char *dest, const char *src) asm("_ft_strcpy");
+int			ft_strcmp(const char *s1, const char *s2) asm("_ft_strcmp");
 char		*ft_strdup(const char *s);
 
 ssize_t		ft_write(int fd, const void *buf, size_t count);
-ssize_t 	ft_read(int fd, void *buf, size_t count);
-
+ssize_t		ft_read(int fd, void *buf, size_t count);
 
 // ft_atoi_base (Comme celui de piscine)•
 // ft_list_push_front (Comme celui de piscine)•
 // ft_list_size (Comme celui de piscine)•
 // ft_list_sort (Comme celui de piscine)•
 // ft_list_remove_if (Comme celui
-
 
 #endif
