0df72f5e81db7181d5a2548af0471d0387ed7be4
ggjulio <juligonz@student.42.fr>
2020-08-13 16:19:15 +0200

    list_sort fixed when list is null

--- srcs/ft_list_sort.s
+++ srcs/ft_list_sort.s
@@ -21,6 +21,10 @@ section .text
 
 ;ft_list_sort(t_list **begin_list, int (*cmp)());
 ft_list_sort:
+cmp qword [rdi], 0
+jne .list_not_null
+	ret
+.list_not_null:
 	push r12	; previous element
 	push r13	; actual
 	push r14	; next
